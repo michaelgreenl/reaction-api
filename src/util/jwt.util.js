317bb3c04c0b838164a765cb307180eeef45bc22
michaelgreenl <lgreener25@gmail.com>
2023-09-15 15:38:44 -0400

    Added a util folder with a jwt file for signing and verifying

---
+++ src/util/jwt.util.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+module.exports = {
+  sign: (obj = {}) =>
+    jwt.sign(obj, JWT_SECRET, {
+      expiresIn: 60*60*24,
+    }),
+  verify: (token) => jwt.verify(token, JWT_SECRET),
+};
