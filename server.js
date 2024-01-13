91dc0b86a5ba2bce08ed1962c8e1159c07f1f203
michaelgreenl <lgreener25@gmail.com>
2024-01-13 13:22:23 -0500

    Made the port variable an environment variable

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({
 const app = require('./src/app');
 const db = require('./src/db/models/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => console.log('server running'));
 });
