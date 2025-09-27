1e0fc5c790b971cb8b41d7525e491c8e04affd01
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-09-27 15:56:50 -0400

    chore: formatting

--- src/util/jwt.util.js
+++ src/util/jwt.util.js
@@ -5,7 +5,7 @@ const { JWT_SECRET } = process.env;
 module.exports = {
   sign: (obj = {}) =>
     jwt.sign(obj, JWT_SECRET, {
-      expiresIn: 60*60*24,
+      expiresIn: 60 * 60 * 24,
     }),
   verify: (token) => jwt.verify(token, JWT_SECRET),
 };
