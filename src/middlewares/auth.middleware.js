b66af0c92d32e72d2e4fbc043953046c70a6885d
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-01 18:18:50 -0400

    feat: changed middleware to use cookies for token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,14 +2,20 @@ const jwt = require('../util/jwt.util');
 const { UNAUTHORIZED } = require('../constants');
 
 module.exports.jwt = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(UNAUTHORIZED).error('Missing token');
+  }
+
   try {
-    const token = req.headers.authorization;
     req.context = {
       ...req.context,
       user: jwt.verify(token),
     };
+
     next();
   } catch (error) {
     res.status(UNAUTHORIZED).error('Unauthorized');
   }
 };
+
