e169e34c6b81750bcf7e1f61384c04ece901d76b
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-01 18:12:37 -0400

    refactor: Changed app to use config from config directory

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./routes');
+const config = require('./config');
 
-const { FRONTEND_URL } = process.env;
-app.use(cors({ origin: FRONTEND_URL }));
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-);
-app.use(routes);
+const app = express();
+
+app.use(config);
 
 module.exports = app;
