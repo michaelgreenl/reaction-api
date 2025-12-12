fb679dd369f94f191423c5d4bf011cb603e7eb66
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-12-12 14:32:25 -0500

    refactor: env variables name change

--- sequelize.config.js
+++ sequelize.config.js
@@ -4,12 +4,12 @@ dotenv.config({
   path: `.env.${environment}`,
 });
 
-const { POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT } = process.env;
+const { POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, PORT } = process.env;
 module.exports = {
   database: POSTGRES_DATABASE,
   dialect: 'postgres',
   host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
+  port: PORT,
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
   dialectOptions:
