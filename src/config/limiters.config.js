2a3423f69e989b2e2fb9e211c90e0a1eccc897c8
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-12-18 17:10:33 -0500

    fix: fixed rate-limiter's max req's and added skip opt for development

--- src/config/limiters.config.js
+++ src/config/limiters.config.js
@@ -1,19 +1,23 @@
 const rateLimit = require('express-rate-limit');
 const slowDown = require('express-slow-down');
 
+const skip = (req) => (process.env.NODE_ENV === 'development' ? true : false);
+
 module.exports.limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 500,
+    max: 1500,
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
+    skip,
 });
 
 module.exports.speedLimiter = slowDown({
     windowMs: 15 * 60 * 1000,
-    delayAfter: 500,
+    delayAfter: 1500,
     delayMs: (used, req) => {
         const delayAfter = req.slowDown.limit;
         return (used - delayAfter) * 500;
     },
+    skip,
 });
